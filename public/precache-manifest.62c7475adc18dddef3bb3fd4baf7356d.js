06d4519167bda114d0c34fd4360584cfe12a2f0a
pablo grillo <pablo.grillo@roiback.com>
2021-10-27 22:04:46 +0200

    fix(public): restore folder

---
+++ public/precache-manifest.62c7475adc18dddef3bb3fd4baf7356d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e1021d10928356c4cfe8d205c68807",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09253bc23c21bcf1f41d",
+    "url": "/static/js/2.ed347f9e.chunk.js"
+  },
+  {
+    "revision": "916f3e69f05e09527ac49c7df326c64c",
+    "url": "/static/js/2.ed347f9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5cb54ed362362dbc8c74",
+    "url": "/static/js/main.e01c3273.chunk.js"
+  },
+  {
+    "revision": "f1648ef6394014a47ca7",
+    "url": "/static/js/runtime-main.f17dec75.js"
+  }
+]);
